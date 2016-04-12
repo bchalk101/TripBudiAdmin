c7cb5c9aa9be3639ffadee45e497ff070a313ad7
Boruch <boruch11@gmail.com>
2016-04-12 14:04:12 +1000

    first commit

---
+++ TripBudiAdmin/AddActivitiesToTripTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddActivitiesToTripTableViewController.h
+//  TripBudiAdmin
+//
+//  Created by Boruch Chalk on 12/04/2016.
+//  Copyright © 2016 bchalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddActivitiesToTripTableViewController : UITableViewController
+
+@end
